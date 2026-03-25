0be1f817becca7eb3368ccaac67a7a23298b08b3
Aleksey Shakhmut <leha.shakhmut@gmail.com>
2026-03-25 10:31:30 +0300

    Добавлены негативные тесты для PATCH и DELETE

--- api-tests/tests/products/products.negative.spec.ts
+++ api-tests/tests/products/products.negative.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '../../fixtures/auth_context';  // для тестов с токеном
 import { faker } from '@faker-js/faker';
-import {createBaseProductFormData, createProductFormData} from "../../utils/form_data_helper";
+import {createBaseProductFormData, createPriceUpdateFormData,} from "../../utils/form_data_helper";
 
 test.describe('Негативные тесты для продуктов', () => {
 
@@ -22,14 +22,6 @@ test.describe('Негативные тесты для продуктов', () =>
         expect(responseBody.errors[5].category).toBe('Invalid category')
      })
 
-    test('GET /products/{id} с несуществующим ID - ожидаем 404', async ({ request }) => {
-        const invalidId = "000000000000000000000000";  // несуществующий ID
-
-        const response = await request.get(`ecommerce/products/${invalidId}`);
-
-        expect(response.status()).toBe(404);
-    });
-
     test('POST /products без авторизации - ожидаем 401', async ({ request }) => {
         const productData = {
             name: faker.commerce.productName(),
@@ -42,6 +34,8 @@ test.describe('Негативные тесты для продуктов', () =>
         });
 
         expect(response.status()).toBe(401);
+        const body = await response.json();
+        expect(body.message).toBe('Unauthorized request');
     });
 
     test('POST /products с неверным Content-Type - ожидаем 400', async ({ request, authToken }) => {
@@ -62,7 +56,7 @@ test.describe('Негативные тесты для продуктов', () =>
         expect(response.status()).toBe(400);
     });
 
-    test('POST /products с неверной price - ожидаем 422', async ({ request, authToken }) => {
+    test('POST /products с неверным типом данных для price - ожидаем 422', async ({ request, authToken }) => {
         const formData = createBaseProductFormData('649865ab297b287175aec1d7')
         formData.append('price', 'сто');
 
@@ -79,4 +73,114 @@ test.describe('Негативные тесты для продуктов', () =>
         expect(errorBody.errors.length).toBe(1);
         expect(errorBody.errors[0].price).toBe('Price must be a number');
     });
+
+    test('GET /products/{id} с несуществующим ID - ожидаем 404', async ({ request }) => {
+        const invalidId = "000000000000000000000000";  // несуществующий ID
+
+        const response = await request.get(`ecommerce/products/${invalidId}`);
+
+        expect(response.status()).toBe(404);
+    });
+
+    test('GET /products/{id} с невалидным ID - ожидаем 422', async ({ request }) => {
+        const invalidId = "0000000000000000000000"; // не хватает двух цифр
+        const response = await request.get(`ecommerce/products/${invalidId}`);
+
+        expect(response.status()).toBe(422);
+
+        const body = await response.json();
+        expect(body.errors.length).toBe(1);
+        expect(body.errors[0].productId).toBe('Invalid productId');
+        expect(body.message).toBe('Received data is not valid');
+    })
+    test('PATCH /products/{productId} с несуществующим ID - ожидаем 404', async ({ request, authToken }) => {
+        const invalidProductId = "000000000000000000000000";
+        const formData = createPriceUpdateFormData('100','000000000000000000000000')
+
+        const response = await request.patch(`ecommerce/products/${invalidProductId}`, {
+            multipart: formData,
+            headers: { 'Authorization': `Bearer ${authToken}` },
+        })
+        expect(response.status()).toBe(404);
+        const body = await response.json();
+        expect(body.message).toBe('Product does not exist');
+    })
+    test('PATCH /products/{productId} с невалидным ID - ожидаем 422', async ({ request, authToken }) => {
+        const invalidProductId = "00000000000000000000000"; // не хватает одной цифры
+        const formData = createPriceUpdateFormData('100','000000000000000000000000')
+
+        const response = await request.patch(`ecommerce/products/${invalidProductId}`, {
+            multipart: formData,
+            headers: { 'Authorization': `Bearer ${authToken}` },
+        })
+        expect(response.status()).toBe(422);
+        const body = await response.json();
+        expect(body.message).toBe('Received data is not valid');
+        expect(body.errors.length).toBe(1);
+        expect(body.errors[0].productId).toBe('Invalid productId');
+    })
+    test('PATCH /products/{productId} без авторизации - ожидаем 401', async ({ request }) => {
+        const ProductId = "000000000000000000000000";
+        const formData = createPriceUpdateFormData('100','000000000000000000000000')
+
+        const response = await request.patch(`ecommerce/products/${ProductId}`, {
+            multipart: formData,
+            // Без Authorization header
+        })
+        expect(response.status()).toBe(401);
+        const body = await response.json();
+        expect(body.message).toBe('Unauthorized request');
+    })
+    test('PATCH /products/{productId} с неверным типом данных для price - ожидаем 422', async ({ request,authToken }) => {
+        const ProductId = "000000000000000000000000";
+        const formData = createPriceUpdateFormData('сто','000000000000000000000000')
+
+        const response = await request.patch(`ecommerce/products/${ProductId}`, {
+            multipart: formData,
+            headers: { 'Authorization': `Bearer ${authToken}` },
+        })
+        expect(response.status()).toBe(422);
+        const body = await response.json();
+        expect(body.message).toBe('Received data is not valid');
+        expect(body.errors.length).toBe(1);
+        expect(body.errors[0].price).toBe('Price must be a number');
+    })
+    test('DELETE /products/{productId} без авторизации - ожидаем 401', async ({ request }) => {
+        const ProductId = "000000000000000000000000";
+        const formData = createPriceUpdateFormData('100','000000000000000000000000')
+
+        const response = await request.delete(`ecommerce/products/${ProductId}`, {
+            multipart: formData,
+            // Без Authorization header
+        })
+        expect(response.status()).toBe(401);
+        const body = await response.json();
+        expect(body.message).toBe('Unauthorized request');
+    })
+    test('DELETE /products/{productId} с несуществующим ID - ожидаем 404', async ({ request, authToken }) => {
+        const invalidProductId = "000000000000000000000000";
+        const formData = createPriceUpdateFormData('100','000000000000000000000000')
+
+        const response = await request.patch(`ecommerce/products/${invalidProductId}`, {
+            multipart: formData,
+            headers: { 'Authorization': `Bearer ${authToken}` },
+        })
+        expect(response.status()).toBe(404);
+        const body = await response.json();
+        expect(body.message).toBe('Product does not exist');
+    })
+    test('DELETE /products/{productId} с невалидным ID - ожидаем 422', async ({ request, authToken }) => {
+        const invalidProductId = "00000000000000000000000"; // не хватает одной цифры
+        const formData = createPriceUpdateFormData('100','000000000000000000000000')
+
+        const response = await request.patch(`ecommerce/products/${invalidProductId}`, {
+            multipart: formData,
+            headers: { 'Authorization': `Bearer ${authToken}` },
+        })
+        expect(response.status()).toBe(422);
+        const body = await response.json();
+        expect(body.message).toBe('Received data is not valid');
+        expect(body.errors.length).toBe(1);
+        expect(body.errors[0].productId).toBe('Invalid productId');
+    })
 });
